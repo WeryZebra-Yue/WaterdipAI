b748a011f1ad298eddc995043d1b2f07cf7effba
WeryZebra-Yue <75676675+WeryZebra-Yue@users.noreply.github.com>
2022-11-17 14:45:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from common.routes import BaseRouter
 app = FastAPI()
 baseRoute = "/v1"
 app.include_router(BaseRouter, prefix=baseRoute)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World", "Docs": "https://waterdip.herokuapp.com/docs", "Base Route": "https://waterdip.herokuapp.com/v1"}
